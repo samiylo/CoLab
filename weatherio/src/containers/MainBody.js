99e3c28c41f9568f377c4c62b6e5b2ac036b46a5
samiylo <skryshu@gmail.com>
2020-09-09 14:27:23 -0500

    finish skeleton

--- weatherio/src/containers/MainBody.js
+++ weatherio/src/containers/MainBody.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import ScanFile from '../components/ScanFile';
-import ScanUrl from './ScanUrl'
+import ScanUrl from './ScanUrl';
+import {Route, Switch} from 'react-router-dom';
 
 export default class MainBody extends Component {
     render() {
         return (
             <div className="MainBody">
-                <ScanFile />
-                <ScanUrl />
+
+                    <ScanFile />
+                    <ScanUrl />
             </div>
         )
     }
