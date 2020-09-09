2b6f27287752403156a6b111b5251aeb9d5619f8
samiylo <skryshu@gmail.com>
2020-09-09 13:18:24 -0500

    start Skeleton:

--- weatherio/src/App.js
+++ weatherio/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ScanFile from './components/ScanFile'
+import Nav from './containers/NavBar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Nav />
+      <ScanFile />
     </div>
   );
 }
---
+++ weatherio/src/components/ScanFile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ScanFile extends Component {
+    render() {
+        return (
+            <div>
+                Scan File
+            </div>
+        )
+    }
+}
+
