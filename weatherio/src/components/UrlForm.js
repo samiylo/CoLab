c6a8671b33de43f9dfd63098206e3749601b64de
samiylo <skryshu@gmail.com>
2020-09-13 16:11:48 -0500

    add UrlForm

--- weatherio/src/components/UrlForm.js
+++ weatherio/src/components/UrlForm.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
 
 export default class UrlForm extends Component {
+
+    state = {
+        URL: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            URL: event.target.value
+        })
+
+        console.log(this.state)
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Submited")
+    }
+
     render() {
         return (
             <div className='UrlForm'>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Input URL</label>
-                    <input type='text'></input>
+                    <input type='text' onChange={this.handleChange}></input>
                     <input type='submit'></input>
                 </form>
                 
