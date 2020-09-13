afbb537343b40ffb5879fa5399450c5b3eb08420
samiylo <skryshu@gmail.com>
2020-09-13 15:20:22 -0500

    add Input Form

--- weatherio/src/components/ScanForm.js
+++ weatherio/src/components/ScanForm.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react'
 export default class ScanForm extends Component {
 
     handleSubmit = () => {
-        console.log(this)
+        console.log("Form Submited")
     }
     render() {
         return (
-            <div>
+            <div className='ScanForm'>
                 <form onSubmit={this.handleSubmit}>
-
+                    <label>Input File</label>
+                    <input type="text"></input>
+                    <input type="submit"></input>
                 </form>
             </div>
         )
--- weatherio/src/containers/MainBody.js
+++ weatherio/src/containers/MainBody.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ScanFile from '../components/ScanFile';
-import ScanUrl from '../components/ScanUrl'
+import ScanUrl from '../components/ScanUrl';
+import ScanForm from '../components/ScanForm';
 import {Route, Switch} from 'react-router-dom';
 
 export default class MainBody extends Component {
     render() {
         return (
             <div className="MainBody">
-
                     <ScanFile />
                     <ScanUrl />
+                    <ScanForm />
             </div>
         )
     }
