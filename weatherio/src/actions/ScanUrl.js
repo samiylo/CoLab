4ee6892c74ba3bf7ba057d0e8dcd32e531b341ed
samiylo <skryshu@gmail.com>
2020-09-14 14:10:15 -0500

    add url scam

--- weatherio/src/actions/ScanUrl.js
+++ weatherio/src/actions/ScanUrl.js
@@ -1,6 +1,12 @@
 export default function (url) {
 
     return (dispatch) => {
-        
+        fetch('https://www.virustotal.com/vtapi/v2/url/scan', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(url)
+        })
     }
 }
