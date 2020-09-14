32eaeadcf5ce8c4632c7371140faa1001fa0d7e2
samiylo <skryshu@gmail.com>
2020-09-14 15:25:44 -0500

    fetch successful

--- weatherio/src/actions/ScanUrl.js
+++ weatherio/src/actions/ScanUrl.js
@@ -1,17 +1,16 @@
 export default function (url) {
 
     // apikey': '899f21daff4d9af4bcec147c4ae9b9b71b8244db0dd91c70ed4c030f266e8c3d'
-    let id = 'https://www.youtube.com'
+    let id = 'aHR0cHM6Ly93d3cueW91dHViZS5jb20'
     return (dispatch) => {
         fetch(`https://www.virustotal.com/api/v3/urls/${id}/analyse`, {
             method: 'POST',
-            mode: 'no-cors',
+            mode: 'cors',
             headers: {
-                'Content-Type' : 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
                 'x-apikey' : '899f21daff4d9af4bcec147c4ae9b9b71b8244db0dd91c70ed4c030f266e8c3d',
-                // 'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: JSON.stringify(id)
+            // body: JSON.stringify(id)
         })
         .then(response => response.json())
         .then(res => console.log(res))
