e397167549db868e4aab4194ae526a705ce883b0
samiylo <skryshu@gmail.com>
2020-09-21 13:22:02 -0500

    remove fetch

--- weatherio/src/components/ScanFile.js
+++ weatherio/src/components/ScanFile.js
@@ -3,14 +3,7 @@ import React, { Component } from 'react'
 export default class ScanFile extends Component {
 
     handleClick = () => {
-        fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02', {
-            method: 'GET',
-            mode: 'cors',
-
-            
-        })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        
     }
     render() {
         return (
