01f24a01596b7dc459421bd1088cf046e207b5c9
samiylo <skryshu@gmail.com>
2020-09-22 11:46:05 -0500

    import NavCon

--- weatherio/src/containers/NavBar.js
+++ weatherio/src/containers/NavBar.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import NavIcon from '../components/NavIcon'
 
 export default class NavBar extends Component {
     render() {
         return (
             <div className="Nav">
-                CoLab
+                NavIcon
             </div>
         )
     }
