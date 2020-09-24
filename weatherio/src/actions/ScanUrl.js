d9e00433ce806dec5682a2f2f1b5aff8fe765938
samiylo <skryshu@gmail.com>
2020-09-24 11:08:15 -0500

    add new

--- weatherio/src/actions/ScanUrl.js
+++ weatherio/src/actions/ScanUrl.js
@@ -1,19 +1,13 @@
-export default function () {
+export default function ScanUrl() {
 
-    let url = 'https://www.youtube.com'
     let apikey = 'AIzaSyAeF4FccVzvriwriD7pI__faA7_lh1zGdQ'
 
-    let data = {apikey, url}
     return (dispatch) => {
-        fetch(`https://www.virustotal.com/vtapi/v2/url/scan`, {
+        fetch(`https://safebrowsing.googleapis.com/v4/threatMatches:find?key=${apikey}`, {
             method: 'POST',
-            mode: 'no-cors',
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'apikey' : '899f21daff4d9af4bcec147c4ae9b9b71b8244db0dd91c70ed4c030f266e8c3d',
-                'url' : 'https://www.youtube.com'
-            },
-            body: data
+                'Content-Type': 'application/json',
+            }
         })
         .then(response => response.json())
         .then(res => console.log(res))
